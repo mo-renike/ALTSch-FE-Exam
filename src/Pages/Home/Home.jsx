d65743a46e5a98026ffceb86424fc4aeb53888f3
Morenike Oyewole <herroyalpianist@gmail.com>
2022-10-25 14:31:01 +0100

    UI adjustment

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,7 +9,8 @@ const Home = () => {
                 {" "}
                 <h1>Finder</h1>
                 <p>
-                    Find your old school mates, ex, collegues, family, friends, debtors or enemy, <br /> find them by their name, gender, age or location.
+                    Find your old school mates, ex, collegues, family, friends, debtors or
+                    enemy, <br /> find them by their name, gender, age or location.
                 </p>
                 <NavLink className="btn" to="/users">
                     Find Them
